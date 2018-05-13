eb2decb0b2c938bdbe32e8e7be6bf0e37e1ff748
Victor <victor.serain@gmx.fr>
2018-05-13 19:42:15 +0200

    [-] fix bug

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,15 @@
 var menuState = {
     create : function() {
         game.add.text(80,80, "Issac is Happy", { font: '50px Arial', fill :'#fff'});
-        game.add.text(80,game.world.height -80, 'press w to start', { font: '25px Arial', fill :'#fff'});
+        game.add.text(80,game.world.height -80, 'press W to start a new game', { font: '25px Arial', fill :'#fff'});
 
         var wKey = game.input.keyboard.addKey(87);
 
-        wKey.onDown.addOnce(this.start, this);
+        wKey.onDown.addOnce(this.startNewGame, this);
     },
-    start: function(){
+    startNewGame: function(){
         console.log("Go");
+        player.create({});
         game.state.start('play');
     }
 }
